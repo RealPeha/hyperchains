355bb8e01a3d7a6166d23c294df7d63e59f129b7
RealPeha <real.peha@gmail.com>
2024-08-07 13:10:34 +0300

    added vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
 import './index.css';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
-  <App />,
+  <>
+    <Analytics />
+    <App />
+  </>,
   // </React.StrictMode>,
 );
